ba22d1048447b5798b2bc9f93909df082ca7a11a
양선우 <@DESKTOP-94O1RKP>
2022-09-19 18:31:38 +0900

    commit 20220919

--- ex03/02.sql/ex02.sql
+++ ex03/02.sql/ex02.sql
@@ -35,6 +35,22 @@ where bno > 0;
 // PK는 식별자, 인덱스 의미 가짐
 
 
+create table tbl_reply (
 
+    rno number(10,0),
+    bno number(10,0) not null,
+    reply varchar2(1000) not null,
+    replyer varchar2(50) not null,
+    replyDate date default sysdate,
+    updateDate date default sysdate
+
+);
+
+create sequence seq_reply;
+
+alter table tbl_reply add constraint pk_reply primary key(rno);
+
+alter table tbl_reply add constraint fk_reply_board
+foreign key (bno) references tbl_board (bno);
 
 
